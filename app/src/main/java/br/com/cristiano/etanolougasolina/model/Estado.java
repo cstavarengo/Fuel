579ec4ff62e4924f0cba4fa8db4dbf78d7885526
Paulo Sergio do Nascimento <pasergio@unicamp.br>
2019-12-05 14:40:07 -0300

    alterações no layout principal

---
+++ app/src/main/java/br/com/cristiano/etanolougasolina/model/Estado.java
@@ -0,0 +1,42 @@
+package br.com.cristiano.etanolougasolina.model;
+
+
+public class Estado {
+
+   private Long id;
+   private String nome;
+   private String sigla;
+
+    public Estado() {
+    }
+
+    public Estado(Long id, String nome, String sigla) {
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+}
